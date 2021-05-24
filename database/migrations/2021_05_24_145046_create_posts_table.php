dac75d1b99f6d430284e79d2b1d11f8541e52283
Giorgio Rindonone <giorgio.rindonone@gmail.com>
2021-05-24 17:07:46 +0200

    crud impostata

---
+++ database/migrations/2021_05_24_145046_create_posts_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePostsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('posts', function (Blueprint $table) {
+            $table->id();
+            //mi credo le voci della tabella Post dentro il db
+            $table->string('title');
+            $table->date('date');
+            $table->text('content');
+            $table->string('image')->nullable();
+            $table->string('slug')->unique();
+            $table->boolean('published')->default(0);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('posts');
+    }
+}
